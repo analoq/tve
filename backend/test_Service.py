ee75c190e606703acd16f91937dfb7d389c4ac16
a. p. matthews <i@apmatthe.ws>
2018-12-23 19:35:40 -0800

    neglect microseconds in test case

--- backend/test_Service.py
+++ backend/test_Service.py
@@ -17,13 +17,14 @@ class TestService(unittest.TestCase):
 
     def test_request(self):
         persistence = Persistence('test.db')
-        dt0 = datetime.utcnow() - timedelta(seconds=30)
+        now = datetime.utcnow().replace(microsecond=0)
+        dt0 = now - timedelta(seconds=30)
         persistence.store(dt0, 100)
         service = Service(persistence)
         response = service.request(self.start_response)
         self.assertEqual(response.next(),
                 'data: [{"dt": "%s", "value": 100}]\n\n' % dt0.isoformat())
-        dt1 = datetime.utcnow()
+        dt1 = now
         service.enqueue({'dt': dt1, 'value': 200})
         self.assertEqual(response.next(),
                 'data: [{"dt": "%s", "value": 200}]\n\n' % dt1.isoformat())
